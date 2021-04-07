6f9414d2d0b0980aaf12ef071e10506f6c3e26a2
Lacey <lacey.griffith@g.austincc.edu>
2021-04-06 23:00:22 -0500

    save answers to questions in new file, successfully prompts user for each quesiton

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+// x TITLE
+// x Description
+// x Installation instructions
+// x Usage Information
+// x Contribution Guidelines
+// x Test instruction
+// x license options
+// x Questions (includes [x] github user-name and [x] link to repo
+// x email address as a contact solution
+
 const fs = require('fs')
-const inquire = require('inquirer')
+const inquirer = require('inquirer')
 
 // const readMeData = process.argv.slice(2)
 // const [githubName, githubProject] = readMeData
@@ -14,47 +24,77 @@ const q2 = {
     message: "Describe your project."
 }
 const q3 = {
-    name: "languages",
-    type: "checkbox",
-    message: "What languages do you know?",
-    choices: ['NODE.JS', 'JAVASCRIPT', 'PYTHON']
+    name: "install",
+    type: "input",
+    message: "What are the installation instructions for this project?",
 }
 const q4 = {
-    name: "github_username",
+    name: "usage",
     type: "input",
-    message: "What is your GitHub username?",
+    message: "What is the usage information?",
 }
-function displayQuestions(array) {}
-const displayQuestions = ([q1,q2,q3,q4]) => {
-    fs.writeFile(filename, JSON.stringify(data, null, '\t'), err => {
-        if(err)
-        return console.log("Error!");
-    });
+const q5 = {
+    name: "contributions",
+    type: "input",
+    message: "Who made contributions?"
+}
+const q6 = {
+    name: "testing",
+    type: "input",
+    message: "What are the testing instructions for your project?"
+}
+const q7 = {
+    name: "licensing",
+    type: "checkbox",
+    message: "Which license would you like to include?",
+    choices: ['MIT', 'Apache', 'Boost', 'Eclipse', 'ISC']
+}
+const q8 = {
+    name: "git-username",
+    type: "input",
+    message: "Please enter your GitHub username."
+}
+const q9 = {
+    name: "git-url",
+    type: "input",
+    message: "Please enter your GitHub project's URL."
+}
+const q10 = {
+    name: "email",
+    type: "input",
+    message: "Please enter your email address."
+}
+
+inquirer.prompt([q1,q2,q3,q4,q5,q6,q7,q8,q9,q10])
+.then( function(data) {
     console.log(data)
-    createPage(data);
-} 
-
-
-const createPage = (data) => {
-        return `
-    # TITLE
-
-    **Technologies**
-
-    **Description**
-    
-    **URL**
-    https://${githubName}.github.io/${githubProject}/
-    
-    ![Site Image](IMAGEPATH)
-    
-    
-    **Contributions**
-    `;
+    fs.writeFile('userInfo.txt', JSON.stringify(data, null, '\t'), err => {
+        if(err) throw err;
     }
+)})
+.catch(err => {console.log("Error")})
 
 
-fs.writeFile('README.md', displayQuestions(data), err => {
-    if (err) throw err;
-    console.log("README created!");
-});
+// const createPage = () => {
+//         return `
+//     # TITLE
+
+//     **Description**
+
+//     **Technologies**
+
+    
+//     **URL**
+    
+//     ![Site Image](IMAGEPATH)
+    
+    
+//     **Contributions**
+//     `;
+//     }
+
+
+// fs.writeFile('README.md', displayQuestions(), err => {
+//     if (err) throw err;
+//     console.log("README created!");
+// });
